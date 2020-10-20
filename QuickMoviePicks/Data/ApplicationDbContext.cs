0410f63620156b4fdd3342eb9e531c9d7e1322ea
ArhumMalik3 <ArhumMalik95@gmail.com>
2020-10-20 16:37:32 -0500

    new repository for QuickPicks, initial commit + I added Identity

---
+++ QuickMoviePicks/Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace QuickMoviePicks.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<IdentityRole>()
+            .HasData(
+            new IdentityRole
+            {
+                Name = "Customer",
+                NormalizedName = "CUSTOMER"
+            }
+            );
+        }
+
+    }
+}
---
+++ QuickMoviePicks/Data/Migrations/20201020211701_Initial.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QuickMoviePicks.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "124e3053-cee8-4646-8b2a-f7a3d233a2d8", "a27e8576-c5d2-43b2-9c5f-aa8534d26754", "Customer", "CUSTOMER" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumns: new[] { "Id", "ConcurrencyStamp" },
+                keyValues: new object[] { "124e3053-cee8-4646-8b2a-f7a3d233a2d8", "a27e8576-c5d2-43b2-9c5f-aa8534d26754" });
+        }
+    }
+}
