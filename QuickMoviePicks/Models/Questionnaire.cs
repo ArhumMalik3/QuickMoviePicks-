305943d611a9ac70f30c30a7c72943820506eeee
ArhumMalik3 <ArhumMalik95@gmail.com>
2020-10-21 01:56:28 -0500

    I added a model for Questionnaire.

---
+++ QuickMoviePicks/Models/Questionnaire.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuickMoviePicks.Models
+{
+    public class Questionnaire
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Genre")]
+        public string genre { get; set; }
+
+        [Display(Name = "Director")]
+        public string director { get; set; }
+
+        [Display(Name = "Actor")]
+        public string actor { get; set; }
+
+        [Display(Name = "Country")]
+        public string country { get; set; }
+    }
+}
