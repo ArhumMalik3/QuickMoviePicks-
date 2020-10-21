ccbcb90551e9087f2ac594caf5b9c76788731f40
ArhumMalik3 <ArhumMalik95@gmail.com>
2020-10-21 00:40:18 -0500

    I added a MovieWatcher Model

---
+++ QuickMoviePicks/Models/MovieWatcher.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuickMoviePicks.Models
+{
+    public class MovieWatcher
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "First Name")]
+        public string firstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string lastName { get; set; }
+
+        [Display(Name = "Country")]
+        public string country { get; set; }
+
+
+
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+    }
+}
