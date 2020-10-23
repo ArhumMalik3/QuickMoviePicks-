5ac5ebe97c935991c93e1cfdba39a008bee402b3
ArhumMalik3 <ArhumMalik95@gmail.com>
2020-10-22 21:00:28 -0500

    added a private api key class and tested api key

--- QuickMoviePickz/Startup.cs
+++ QuickMoviePickz/Startup.cs
@@ -45,6 +45,7 @@ namespace QuickMoviePickz
             });
             services.AddControllersWithViews();
             services.AddRazorPages();
+            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
